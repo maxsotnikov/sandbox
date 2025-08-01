98f23c9f7752d686010d234431184de2596f40c6
Max Sotnikov <maxsotnikov00@gmail.com>
2025-08-01 18:54:53 +0300

    tasks

---
+++ 8kyu/multiply.js
@@ -0,0 +1,12 @@
+//🧾DESCRIPTION
+// This code does not execute properly. Try to figure out why.
+// function multiply(a, b){
+//   a * b
+// }
+
+//✅SOLUTION;
+function multiply(a, b){
+  return a * b
+}
+
+console.log(multiply(4,6));
