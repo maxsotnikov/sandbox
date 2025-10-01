07c165a3c297ad5677796813ce12b9d7ab8b15c5
Max Sotnikov <maxsotnikov00@gmail.com>
2025-10-01 15:14:22 +0300

    solution

---
+++ 8kyu/square(n)-sum.js
@@ -0,0 +1,15 @@
+/*🧾DESCRIPTION
+Complete the square sum function so that it squares each number passed into it and then sums the results together.
+
+For example, for [1, 2, 2] it should return 9
+-------------------------------------------------------------------
+*/
+
+//✅SOLUTION:
+function squareSum(numbers){
+  let sum = 0;
+  numbers.forEach(element => {  
+    sum += (element ** 2);
+  });
+  return sum;
+}
