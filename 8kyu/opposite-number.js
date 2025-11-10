525a0bfc99c6587385ced41819ce86761675aaeb
Max Sotnikov <maxsotnikov00@gmail.com>
2025-11-10 17:48:45 +0300

    solution

---
+++ 8kyu/opposite-number.js
@@ -0,0 +1,20 @@
+/*🧾DESCRIPTION
+Very simple, given a number, find its opposite (additive inverse).
+
+Examples:
+
+  1: -1
+14: -14
+-34: 34
+-------------------------------------------------------------------
+*/
+
+//✅SOLUTION1️⃣:
+function opposite(number) {
+  return number > 0 || number < 0 ? -number : number
+}
+
+//✅SOLUTION2️⃣:
+// function opposite(number) {
+//   // return -number
+// }
